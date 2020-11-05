e8528e095befafcf30b340a75b51fa4bbb07cd8e
N Balgopal Patro <n.balgopalpatra@gmail.com>
2020-11-05 17:28:12 +0530

    Initial Commit

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function _404() {
+    return <h1>404</h1>
+}
+
+export default _404;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <a href="/Dashboard">Dashboard</a>
+        </div>
+    )
+
+}
+
+export default HelloWorld;
